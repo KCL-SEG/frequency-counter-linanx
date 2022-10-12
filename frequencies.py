d262db5de9fe88d6ea3f771b97ac7d410175f3c3
Lina Namoun <76211897+linanx@users.noreply.github.com>
2022-10-12 20:09:38 +0100

    Complete task

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for i in items:
+        if str(i) in frequencies:
+            frequencies[str(i)] += 1
+        else:
+            frequencies[str(i)] = 1
+            
     return frequencies
